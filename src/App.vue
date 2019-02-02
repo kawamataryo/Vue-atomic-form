765a1ac18e8269f6913128bf6a5f906fccbffead
ryo <ba068082@gmail.com>
2019-02-02 13:31:16 +0900

    add: inputText

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div id="app">
-    <form action=""></form>
+    <form action="">
+      <MyInput v-model="sampleForm.text" label="テキストエリア"></MyInput>
+    </form>
   </div>
 </template>
 
 <script>
+import MyInput from "@/components/MyInput";
+
 export default {
   name: "app",
-  components: {},
+  components: {
+    MyInput
+  },
   data() {
     return {
       sampleForm: {
         text: "",
         radio: "",
         select: "",
-        textarea: ""
+        textarea: "",
+        file: ""
       }
     };
   }
---
+++ src/components/MyInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <label>
+    {{ label }}
+    <input
+      type="text"
+      :name="name"
+      :value="value"
+      :placeholder="placeholder"
+      @input="updateValue"
+    />
+  </label>
+</template>
+
+<script>
+export default {
+  name: "MyInput",
+  props: {
+    value: { type: String, require: true },
+    name: { type: String, require: true },
+    label: { type: String, require: true },
+    placeholder: { type: String, require: false }
+  },
+  methods: {
+    updateValue: function(e) {
+      this.$emit("input", e.target.value);
+    }
+  }
+};
+</script>
+
+<style scoped lang="stylus">
+
+</style>
